93c39353e6bef1742ceebea450f662d79f4f60fb
natuta <natalilinomendoza@gmail.com>
2021-03-05 11:46:05 -0400

    Updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -21,8 +24,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
-    }
+    public function boot(UrlGenerator $url)
+{
+    Schema::defaultStringLength(191);
+   if (env('REDIRECT_HTTPS')) {
+       $url->formatScheme('https://');
+   }
+}
+   
 }

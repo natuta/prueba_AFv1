dd598b891f32f0020bfc3c2a1bd014b47d0ead01
natuta <natalilinomendoza@gmail.com>
2021-03-12 07:48:20 -0400

    Updated

--- resources/views/bar-chart.blade.php
+++ resources/views/bar-chart.blade.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     </head>
     <body>
         <canvas id="barChart" width="400" height="400"></canvas>

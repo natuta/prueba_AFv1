697ea35b51d7f26f341afc751a20a1fc5b927486
natuta <natalilinomendoza@gmail.com>
2021-03-15 11:19:47 -0400

    Updated

--- resources/views/bar-chart.blade.php
+++ resources/views/bar-chart.blade.php
@@ -180,8 +180,6 @@ $('#downloadPdf').click(function(event) {
       
       <button type="button" id="downloadPdf"> Download Higher Quality PDF </button>  
 
-        <a href="#" id="downloadPdf">Download Report Page as PDF</a>
-        <br/><br/>
         <div id="reportPage">
         <div id="chartContainer" style="width: 30%;float: left;">
             <canvas id="myChart"></canvas>

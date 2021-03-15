c1deb2db7ee121976878bec930760bc5b03f7234
natuta <natalilinomendoza@gmail.com>
2021-03-15 00:30:47 -0400

    Updated

--- resources/views/bar-chart.blade.php
+++ resources/views/bar-chart.blade.php
@@ -10,12 +10,39 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<style>
+#chartContainer {
+    width: 40%
+  };
+  
+  #myChart2 {
+    width: 40%
+  };</style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     </head>
     <body>
         <div class="col-lg-12" style="padding-top:20px;">
             <div class="card">
-                <div class="card-header center">Reportes Graficos</div>
+                <div class="card-header center">Reportes Graficos
+
+                        <a href="#" id="downloadPdf">Download Report Page as PDF</a>
+                        <br/><br/>
+                        <div id="reportPage">
+                        <div id="chartContainer" style="width: 30%;float: left;">
+                            <canvas id="myChart"></canvas>
+                        </div>
+
+                        <div style="width: 30%; float: left;">
+                            <canvas id="myChart2"></canvas>
+                        </div>
+
+                        <br/><br/><br/>
+
+                        <div style="width: 30%; height: 400px; clear: both;">
+                            <canvas id="myChart3" style="width: 40%"></canvas>
+                        </div>
+                        </div>
+                </div>
                  
                 <div class="card-body">
                      <div class="row"> 
@@ -158,6 +185,7 @@ $(function(){
                 datasets:[{
                         label:'Incremento de nuevos Usuarios,2021',
                         data:datas,
+                        borderWidth:5,
                         backgroundColor:datas1
                 }]
             },
@@ -174,4 +202,159 @@ $(function(){
             }
         })
         });
+</script>
+<script>
+
+    var chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(231,233,237)'
+};
+
+var randomScalingFactor = function() {
+  return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 100);
+};
+
+var data =  {
+  labels: ["Car", "Bike", "Walking"],
+  datasets: [{
+    label: 'Fuel',
+    backgroundColor: [
+      chartColors.red,
+      chartColors.blue,
+      chartColors.yellow],
+    data: [
+      randomScalingFactor(), 
+      randomScalingFactor(), 
+      randomScalingFactor(), 
+    ]
+  }]
+};
+
+var myBar = new Chart(document.getElementById("myChart"), {
+  type: 'horizontalBar', 
+  data: data, 
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Chart.js - Base Example"
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+var myBar2 = new Chart(document.getElementById("myChart2"), {
+  type: 'horizontalBar', 
+  data: data, 
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Chart.js - Changing X Axis Step Size"
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize: 2
+        }
+      }]
+    }
+  }
+});
+
+var myBar3 = new Chart(document.getElementById("myChart3"), {
+  type: 'horizontalBar', 
+  data: data, 
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+      display: true,
+      text: "Chart.js - Setting maintainAspectRatio = false and Setting Parent Width/Height"
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+$('#downloadPdf').click(function(event) {
+  // get size of report page
+  var reportPageHeight = $('#reportPage').innerHeight();
+  var reportPageWidth = $('#reportPage').innerWidth();
+  
+  // create a new canvas object that we will populate with all other canvas objects
+  var pdfCanvas = $('<canvas />').attr({
+    id: "canvaspdf",
+    width: reportPageWidth,
+    height: reportPageHeight
+  });
+  
+  // keep track canvas position
+  var pdfctx = $(pdfCanvas)[0].getContext('2d');
+  var pdfctxX = 0;
+  var pdfctxY = 0;
+  var buffer = 100;
+  
+  // for each chart.js chart
+  $("canvas").each(function(index) {
+    // get the chart height/width
+    var canvasHeight = $(this).innerHeight();
+    var canvasWidth = $(this).innerWidth();
+    
+    // draw the chart into the new canvas
+    pdfctx.drawImage($(this)[0], pdfctxX, pdfctxY, canvasWidth, canvasHeight);
+    pdfctxX += canvasWidth + buffer;
+    
+    // our report page is in a grid pattern so replicate that in the new canvas
+    if (index % 2 === 1) {
+      pdfctxX = 0;
+      pdfctxY += canvasHeight + buffer;
+    }
+  });
+  
+  // create new pdf and add our new canvas as an image
+  var pdf = new jsPDF('l', 'pt', [reportPageWidth, reportPageHeight]);
+  pdf.addImage($(pdfCanvas)[0], 'PNG', 0, 0);
+  
+  // download the pdf
+  pdf.save('filename.pdf');
+});
 </script>

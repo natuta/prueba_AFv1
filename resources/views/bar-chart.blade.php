96ce8f2b58aed672a53da01fdea406220f633360
natuta <natalilinomendoza@gmail.com>
2021-03-15 10:55:36 -0400

    Updated

--- resources/views/bar-chart.blade.php
+++ resources/views/bar-chart.blade.php
@@ -8,59 +8,169 @@
 button {/*from ww  w .  jav  a 2  s  .c  o m*/
    margin-top:30px; padding:10px 20px; border-radius:0;
 }
+#chartContainer {
+  width: 40%
+};
+
+#myChart2 {
+  width: 40%
+};
       </style> 
       <script type="text/javascript">
     window.onload=function(){
-        var datas31= <?php echo json_encode($categorias3);?>;
-         var datas32= <?php echo json_encode($valores3);?>;
-         var datas33= <?php echo json_encode($colores3);?>;
-         var canvas = document.querySelector('#cool-canvas');
-         var barChart3 = new Chart(canvas,{
-        type:"horizontalBar",
-        
-        data:{
-            labels:datas31,
-            datasets:[{
-                    label:'Costo de Mantenimientos, por Categoria',
-                    data:datas32,
-                    backgroundColor:datas33
-                   
-                        
-            }]
-        },
-        options:{
-            scales:{
-                yAxes:[{
-                        ticks:{
-                            beginAtZero:true
-                        }
-                }]
-            },
-            title: {
-            display: true,
-            text: 'Mantenimientos'
+        var chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(231,233,237)'
+};
+
+var randomScalingFactor = function() {
+  return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 100);
+};
+
+var data =  {
+  labels: ["Car", "Bike", "Walking"],
+  datasets: [{
+    label: 'Fuel',
+    backgroundColor: [
+      chartColors.red,
+      chartColors.blue,
+      chartColors.yellow],
+    data: [
+      randomScalingFactor(), 
+      randomScalingFactor(), 
+      randomScalingFactor(), 
+    ]
+  }]
+};
+
+var myBar = new Chart(document.getElementById("myChart"), {
+  type: 'horizontalBar', 
+  data: data, 
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Chart.js - Base Example"
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true
         }
-        }
-    })
-//original canvas
-
-//hidden canvas
-
-//add event listener to button
-document.getElementById('download-pdf').addEventListener("click", downloadPDF);
-//donwload pdf from original canvas
-function downloadPDF() {
-  var canvas = document.querySelector('#cool-canvas');
-   var canvasImg = canvas.toDataURL("image/jpeg", 1.0);
-   var doc = new jsPDF('landscape');
-   doc.setFontSize(20);
-   doc.text(15, 15, "Cool Chart");
-   doc.addImage(canvasImg, 'JPEG', 10, 10, 280, 150 );
-   doc.save('canvas.pdf');
-}
-
+      }]
     }
+  }
+});
 
+var myBar2 = new Chart(document.getElementById("myChart2"), {
+  type: 'horizontalBar', 
+  data: data, 
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Chart.js - Changing X Axis Step Size"
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize: 2
+        }
+      }]
+    }
+  }
+});
+
+var myBar3 = new Chart(document.getElementById("myChart3"), {
+  type: 'horizontalBar', 
+  data: data, 
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+      display: true,
+      text: "Chart.js - Setting maintainAspectRatio = false and Setting Parent Width/Height"
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+$('#downloadPdf').click(function(event) {
+  // get size of report page
+  var reportPageHeight = $('#reportPage').innerHeight();
+  var reportPageWidth = $('#reportPage').innerWidth();
+  
+  // create a new canvas object that we will populate with all other canvas objects
+  var pdfCanvas = $('<canvas />').attr({
+    id: "canvaspdf",
+    width: reportPageWidth,
+    height: reportPageHeight
+  });
+  
+  // keep track canvas position
+  var pdfctx = $(pdfCanvas)[0].getContext('2d');
+  var pdfctxX = 0;
+  var pdfctxY = 0;
+  var buffer = 100;
+  
+  // for each chart.js chart
+  $("canvas").each(function(index) {
+    // get the chart height/width
+    var canvasHeight = $(this).innerHeight();
+    var canvasWidth = $(this).innerWidth();
+    
+    // draw the chart into the new canvas
+    pdfctx.drawImage($(this)[0], pdfctxX, pdfctxY, canvasWidth, canvasHeight);
+    pdfctxX += canvasWidth + buffer;
+    
+    // our report page is in a grid pattern so replicate that in the new canvas
+    if (index % 2 === 1) {
+      pdfctxX = 0;
+      pdfctxY += canvasHeight + buffer;
+    }
+  });
+  
+  // create new pdf and add our new canvas as an image
+  var pdf = new jsPDF('l', 'pt', [reportPageWidth, reportPageHeight]);
+  pdf.addImage($(pdfCanvas)[0], 'PNG', 0, 0);
+  
+  // download the pdf
+  pdf.save('filename.pdf');
+})
+    });
       </script> 
    </head> 
    <body> 
@@ -72,5 +182,23 @@ function downloadPDF() {
       </div> 
       <button type="button" id="download-pdf"> Download PDF </button> 
       <button type="button" id="download-pdf2"> Download Higher Quality PDF </button>  
+
+        <a href="#" id="downloadPdf">Download Report Page as PDF</a>
+        <br/><br/>
+        <div id="reportPage">
+        <div id="chartContainer" style="width: 30%;float: left;">
+            <canvas id="myChart"></canvas>
+        </div>
+
+        <div style="width: 30%; float: left;">
+            <canvas id="myChart2"></canvas>
+        </div>
+
+        <br/><br/><br/>
+
+        <div style="width: 30%; height: 400px; clear: both;">
+            <canvas id="myChart3" style="width: 40%"></canvas>
+        </div>
+        </div>
    </body>
 </html>

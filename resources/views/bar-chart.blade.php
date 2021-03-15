a0fe80ffdd80d745f7d2744f70c8c470c3bf7945
natuta <natalilinomendoza@gmail.com>
2021-03-15 06:24:31 -0400

    Updated

--- resources/views/bar-chart.blade.php
+++ resources/views/bar-chart.blade.php
@@ -1,208 +1,77 @@
-
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+   <head> 
+      <title>Chart.js and jsPDF</title> 
+      <meta name="viewport" content="width=device-width, initial-scale=1"> 
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js"></script> 
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script> 
+      <style id="compiled-css" type="text/css">
+button {/*from ww  w .  jav  a 2  s  .c  o m*/
+   margin-top:30px; padding:10px 20px; border-radius:0;
+}
+      </style> 
+      <script type="text/javascript">
+    window.onload=function(){
+var chart_data = {
+         labels: ['Player1', 'Player2', 'Player3', 'Player4'],
+         datasets: [
+             {
+                 fillColor: "rgba(6, 118, 152, 0.71)",
+                 strokeColor: "rgba(220,220,220,1)",
+                 pointColor: "rgba(220,220,220,1)",
+                 pointStrokeColor: "#fff",
+                 pointHighlightFill: "#fff",
+                 pointHighlightStroke: "rgba(220,220,220,1)",
+                 data: [20,34,15,64,]
+             }
+         ]
+}
+//original canvas
+var canvas = document.querySelector('#cool-canvas');
+var context = canvas.getContext('2d');
+new Chart(context).Line(chart_data);
+//hidden canvas
+var newCanvas = document.querySelector('#supercool-canvas');
+newContext = newCanvas.getContext('2d');
+var supercoolcanvas = new Chart(newContext).Line(chart_data);
+supercoolcanvas.defaults.global = {
+   scaleFontSize: 600
+}
+//add event listener to button
+document.getElementById('download-pdf').addEventListener("click", downloadPDF);
+//donwload pdf from original canvas
+function downloadPDF() {
+  var canvas = document.querySelector('#cool-canvas');
+   var canvasImg = canvas.toDataURL("image/jpeg", 1.0);
+   var doc = new jsPDF('landscape');
+   doc.setFontSize(20);
+   doc.text(15, 15, "Cool Chart");
+   doc.addImage(canvasImg, 'JPEG', 10, 10, 280, 150 );
+   doc.save('canvas.pdf');
+}
 
-     
-       
-        <!-- JavaScript Bundle with Popper -->
+document.getElementById('download-pdf2').addEventListener("click", downloadPDF2);
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.3/jspdf.debug.js"></script>
+function downloadPDF2() {
+   var newCanvas = document.querySelector('#supercool-canvas');
+   var newCanvasImg = newCanvas.toDataURL("image/jpeg", 1.0);
+   var doc = new jsPDF('landscape');
+   doc.setFontSize(20);
+   doc.text(15, 15, "Super Cool Chart");
+   doc.addImage(newCanvasImg, 'JPEG', 10, 10, 280, 150 );
+   doc.save('new-canvas.pdf');
+ }
+    }
 
-    <style>
-        #chartContainer {
-            width: 40%
-          };
-          
-          #myChart2 {
-            width: 40%
-          };</style>
-</head>
-    <body>
-        
-
-                        <a href="#" id="downloadPdf" >Download Report Page as PDF</a>
-                        <br/><br/>
-                        <div id="reportPage">
-                        <div id="chartContainer" style="width: 30%;float: left;">
-                            <canvas id="myChart"></canvas>
-                        </div>
-
-                        <div style="width: 30%; float: left;">
-                            <canvas id="myChart2"></canvas>
-                        </div>
-
-                        <br/><br/><br/>
-
-                        <div style="width: 30%; height: 400px; clear: both;">
-                            <canvas id="myChart3" style="width: 40%"></canvas>
-                        </div>
-                        </div>
-              
-    </body>
-     
+      </script> 
+   </head> 
+   <body> 
+      <div> 
+         <canvas id="cool-canvas" width="600" height="300"></canvas> 
+      </div> 
+      <div style="height:0; width:0; overflow:hidden;"> 
+         <canvas id="supercool-canvas" width="1200" height="600"></canvas> 
+      </div> 
+      <button type="button" id="download-pdf"> Download PDF </button> 
+      <button type="button" id="download-pdf2"> Download Higher Quality PDF </button>  
+   </body>
 </html>
-
-<script>
-
-    var chartColors = {
-  red: 'rgb(255, 99, 132)',
-  orange: 'rgb(255, 159, 64)',
-  yellow: 'rgb(255, 205, 86)',
-  green: 'rgb(75, 192, 192)',
-  blue: 'rgb(54, 162, 235)',
-  purple: 'rgb(153, 102, 255)',
-  grey: 'rgb(231,233,237)'
-};
-
-var randomScalingFactor = function() {
-  return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 100);
-};
-
-var data =  {
-  labels: ["Car", "Bike", "Walking"],
-  datasets: [{
-    label: 'Fuel',
-    backgroundColor: [
-      chartColors.red,
-      chartColors.blue,
-      chartColors.yellow],
-    data: [
-      randomScalingFactor(), 
-      randomScalingFactor(), 
-      randomScalingFactor(), 
-    ]
-  }]
-};
-
-var myBar = new Chart(document.getElementById("myChart"), {
-  type: 'horizontalBar', 
-  data: data, 
-  options: {
-    responsive: true,
-    title: {
-      display: true,
-      text: "Chart.js - Base Example"
-    },
-    tooltips: {
-      mode: 'index',
-      intersect: false
-    },
-    legend: {
-      display: false,
-    },
-    scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-});
-
-var myBar2 = new Chart(document.getElementById("myChart2"), {
-  type: 'horizontalBar', 
-  data: data, 
-  options: {
-    responsive: true,
-    title: {
-      display: true,
-      text: "Chart.js - Changing X Axis Step Size"
-    },
-    tooltips: {
-      mode: 'index',
-      intersect: false
-    },
-    legend: {
-      display: false,
-    },
-    scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true,
-          stepSize: 2
-        }
-      }]
-    }
-  }
-});
-
-var myBar3 = new Chart(document.getElementById("myChart3"), {
-  type: 'horizontalBar', 
-  data: data, 
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    title: {
-      display: true,
-      text: "Chart.js - Setting maintainAspectRatio = false and Setting Parent Width/Height"
-    },
-    tooltips: {
-      mode: 'index',
-      intersect: false
-    },
-    legend: {
-      display: false,
-    },
-    scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-});
-</script>
-    <script>
-$('#downloadPdf').click(function(event) {
-  // get size of report page
-  var reportPageHeight = $('#reportPage').innerHeight();
-  var reportPageWidth = $('#reportPage').innerWidth();
-  
-  // create a new canvas object that we will populate with all other canvas objects
-  var pdfCanvas = $('<canvas />').attr({
-    id: "canvaspdf",
-    width: reportPageWidth,
-    height: reportPageHeight
-  });
-  
-  // keep track canvas position
-  var pdfctx = $(pdfCanvas)[0].getContext('2d');
-  var pdfctxX = 0;
-  var pdfctxY = 0;
-  var buffer = 100;
-  
-  // for each chart.js chart
-  $("canvas").each(function(index) {
-    // get the chart height/width
-    var canvasHeight = $(this).innerHeight();
-    var canvasWidth = $(this).innerWidth();
-    
-    // draw the chart into the new canvas
-    pdfctx.drawImage($(this)[0], pdfctxX, pdfctxY, canvasWidth, canvasHeight);
-    pdfctxX += canvasWidth + buffer;
-    
-    // our report page is in a grid pattern so replicate that in the new canvas
-    if (index % 2 === 1) {
-      pdfctxX = 0;
-      pdfctxY += canvasHeight + buffer;
-    }
-  });
-  
-  // create new pdf and add our new canvas as an image
-  var pdf = new jsPDF('l', 'pt', [reportPageWidth, reportPageHeight]);
-  pdf.addImage($(pdfCanvas)[0], 'PNG', 0, 0);
-  
-  // download the pdf
-  pdf.save('filename.pdf');
-});
-</script>
